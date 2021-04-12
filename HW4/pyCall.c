c11474c761280586a408a30eb971ee52c9d25cb6
PPPy290351 <testforssid03@gmail.com>
2021-04-12 00:49:37 -0700

    fix checking for accessing to kernel and denied it

--- HW4/pyCall.c
+++ HW4/pyCall.c
@@ -7,13 +7,17 @@ asmlinkage long sys_chishen(char * inputStr)
     int len = strlen(inputStr);
     if(len > 100)
         len = 100;
-    while(!access_ok(VERIFY_READ, inputStr, len))
-        len--;
+    while(!access_ok(VERIFY_READ, inputStr, len)){
+        printk("Access denied\n");
+        return -1;
+    }
     char buffer[100];
     int i=0;
     for(;i<len;i++)
         buffer[i] = *(inputStr+i);
-    buffer[i] = '\0';
+    if(i == 100){
+        buffer[i] = '\0';
+    }
     printk("message: %s\n", buffer);
     return 0;
 }
