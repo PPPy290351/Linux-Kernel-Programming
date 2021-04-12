f145ef60471c02a6e79fed9d7716127e925fbaa4
PPPy290351 <testforssid03@gmail.com>
2021-04-12 02:37:17 -0700

    updating

--- HW4/pyCall.c
+++ HW4/pyCall.c
@@ -7,10 +7,12 @@ asmlinkage long sys_chishen(char * inputStr)
     int len = strlen(inputStr);
     if(len > 100)
         len = 100;
+    printk("======================= pointer verifier ======================================\n");
     while(!access_ok(VERIFY_READ, inputStr, len)){
         printk("Access denied\n");
         return -1;
     }
+    printk("=============================================================\n");
     char buffer[100];
     int i=0;
     for(;i<len;i++)
